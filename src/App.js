92c702435c379fae405bb44a29a2d1d7172b4260
Justin Moriarty <75666702+justin2268@users.noreply.github.com>
2023-06-18 09:59:09 +1000

    adding content to main page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Justin Moriarty</h1>
+        <p>I like to solve problems and drink coffee.</p>
+        <p>Cycling is my sport.</p>
       </header>
     </div>
   );
